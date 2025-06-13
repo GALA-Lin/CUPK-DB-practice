d44774c9bf80a48d9a785295eebbd621ece1b3ac
Linsen HU <1563883475@qq.com>
2025-06-13 12:17:18 +0800

    init

---
+++ zyxt/src/main/java/JDBCUtil.java
@@ -0,0 +1,32 @@
+import java.sql.*;
+
+public class JDBCUtil {
+    private static final String URL = "jdbc:mysql://localhost:3306/zyxt";
+    private static final String USER = "root";
+    private static final String PASSWORD = "041225";
+
+    // 加载驱动
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // 获取连接
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+
+    // 关闭资源
+    public static void close(Connection conn, PreparedStatement ps, ResultSet rs) {
+        try {
+            if (rs != null) rs.close();
+            if (ps != null) ps.close();
+            if (conn != null) conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
